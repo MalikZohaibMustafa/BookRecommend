df666b3d3c516dd07daba9b815e5ea2e83f4e746
Malik <zohaibmalik964242@gmail.com>
2023-12-15 13:35:07 +0500

    first commit

---
+++ model_2.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import pickle
+from flask import Flask, render_template, request, redirect, url_for
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # This will enable CORS for all routes
+
+# Load the pickled 'score' and 'table' data
+with open('score.pkl', 'rb') as score_file:
+    score = pickle.load(score_file)
+
+with open('table.pkl', 'rb') as table_file:
+    table = pickle.load(table_file)
+
+def book_ML(name):
+    recommendation = []
+    index = np.where(table.index == name)[0][0]
+    dis = sorted(list(enumerate(score[index])), key=lambda x: x[1], reverse=True)[1:10]
+    for i in dis:
+        recommendation.append(table.index[i[0]])
+    return recommendation
+
+
+@app.route('/recommendations/<book_title>', methods=['GET'])
+def recommendations(book_title):
+    recommendations = book_ML(book_title)
+    return {"recommendations": recommendations, "user_input": book_title}
+
+    return render_template('recommendations.html', book_title=book_title, recommendations=recommendations)
+
+if __name__ == '__main__':
+    app.run(debug=True)
